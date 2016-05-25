019709d7a20f1c68a3682e09dc9145e6e52851e8
Jordan <savage@savagej.com>
2016-05-25 07:55:46 -0700

    Initial Commit

---
+++ blocbrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blocbrowser
+//
+//  Created by Jordan Carlson on 5/25/16.
+//  Copyright © 2016 savagej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blocbrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blocbrowser
+//
+//  Created by Jordan Carlson on 5/25/16.
+//  Copyright © 2016 savagej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
